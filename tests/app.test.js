7e02e35d3bff64af74d5325f3af0ed3c0944733e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-16 13:19:46 -0700

    I have two routes with passing tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-// const Meme = require('../lib/models/Meme');
+const Meme = require('../lib/models/Meme');
 
 describe('meme route tests', () => {
     beforeAll(() => {
@@ -21,14 +21,30 @@ describe('meme route tests', () => {
     it('can create a new meme', () => {
         return request(app)
             .post('/api/v1/memes')
-            .send({ image: 'an image url' })
+            .send({ image: 'an image url', topText: 'witty text', bottomText: 'even wittier!' })
             .then(res => {
                 expect(res.body).toEqual({
                     _id: expect.any(String),
+                    image: 'an image url',
                     topText: 'witty text',
                     bottomText: 'even wittier!',
                     __v: 0
                 });
+            });
+    });
+
+    it('can get all memes', async() => {
+        const meme = await Meme.create({ 
+            image: 'an image url', 
+            topText: 'witty text', 
+            bottomText: 'even wittier!' 
+        });
+        
+        return request(app)
+            .get('/api/v1/memes')
+            .then(res => {
+                const memeJSON = JSON.parse(JSON.stringify(meme));
+                expect(res.body).toEqual([memeJSON]);
             });
     });
 });
