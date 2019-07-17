e7953088914535a198f41250545aa22568bf7e19
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-17 11:43:07 -0700

    adding more to the exercise

--- tests/dog.test.js
+++ tests/dog.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Person = require('../lib/models/Person');
 const Dog = require('../lib/models/Dog');
 
 describe('dog routes tests', () => {
@@ -12,6 +13,11 @@ describe('dog routes tests', () => {
 
     beforeEach(() => {
         return mongoose.connection.dropDatabase();
+    });
+
+    let owner = null;
+    beforeEach(async() => {
+        owner = JSON.parse(JSON.stringify(await Person.create({ name: 'ryan', email: 'ryan@ryan.com' })));
     });
 
     afterAll(() => {
@@ -24,7 +30,8 @@ describe('dog routes tests', () => {
             .send({
                 name: 'Pennie',
                 age: 7,
-                weight: '12 lbs'
+                weight: '12 lbs',
+                owner: owner._id
             })
             .then(res => {
                 expect(res.body).toEqual({
@@ -32,23 +39,81 @@ describe('dog routes tests', () => {
                     name: 'Pennie',
                     age: 7,
                     weight: '12 lbs',
+                    owner: expect.any(String),
                     __v: 0
                 });
             });
     });
 
     it('can return all dogs', async() => {
-        const dog = await Dog.create({
+        const dog = await Dog.create([{
             name: 'Pennie',
             age: 7,
-            weight: '12 lbs'
-        });
+            weight: '12 lbs',
+            owner: owner._id
+        }],
+        {
+            name: 'Charolette',
+            age: 10,
+            weight: '40 lbs',
+            owner: owner._id
+        }
+        );
 
         return request(app)
             .get('/api/v1/dogs')
             .then(res => {
+                const dogsJSON = JSON.parse(JSON.stringify(dog));
+                dogsJSON.forEach(dog => {
+                    expect(res.body).toContainEqual(dog);
+                });
+            });
+    });
+
+    it('can return a dog with their owner by id', async() => {
+        const dog = await Dog.create({
+            name: 'Charolette',
+            age: 10,
+            weight: '40 lbs',
+            owner: owner._id
+        });
+
+        return request(app)
+            .get(`/api/v1/dogs/${dog._id}`)
+            .then(res => {
                 const dogJSON = JSON.parse(JSON.stringify(dog));
-                expect(res.body).toEqual([dogJSON]);
+                expect(res.body).toEqual({
+                    ...dogJSON,
+                    owner
+                });
+            });
+    });
+
+    it('can update a dog by id', async() => {
+        const dog = await Dog.create({
+            name: 'Charolette',
+            age: 10,
+            weight: '40 lbs',
+            owner: owner._id
+        });
+
+        return request(app)
+            .put(`/api/v1/dogs/${dog._id}`)
+            .send({
+                name: 'Pennie',
+                age: 7,
+                weight: '12 lbs',
+                owner: owner._id
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    name: 'Pennie',
+                    age: 7,
+                    weight: '12 lbs',
+                    owner: owner._id,
+                    __v: 0
+                });
             });
     });
 });
